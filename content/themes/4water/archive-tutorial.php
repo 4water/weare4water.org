f344737cfd439e601e4a28577eedde87f9dd576d
Iain Porter <iporter@intraspin.com>
2014-01-20 09:09:10 +0000

    Removing Kint Debugger Supporting pagination of videos Introducing a comprehensive .gitignore file

--- content/themes/4water/archive-tutorial.php
+++ content/themes/4water/archive-tutorial.php
@@ -9,4 +9,12 @@
 		<?php get_template_part('templates/content', get_post_type()); ?>
 		<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 	<?php endwhile; ?>
+	<?php if ($wp_query->max_num_pages > 1) : ?>
+		<nav class="post-nav">
+			<ul class="pager">
+				<li class="previous"><?php next_posts_link(__('Older Videos', 'roots')); ?></li>
+				<li class="next"><?php previous_posts_link(__('Newer Videos', 'roots')); ?></li>
+			</ul>
+		</nav>
+	<?php endif; ?>
 </section>
--- content/themes/4water/templates/content-tutorial.php
+++ content/themes/4water/templates/content-tutorial.php
@@ -1,5 +1,12 @@
 <div class="item">
 	<iframe width="560" height="315" src="http://www.youtube.com/embed/<?php the_field('video_code'); ?>" frameborder="0" allowfullscreen></iframe>
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
+	<?php
+		if (is_single()) :
+			?><h2><?php the_title(); ?></h2><?php
+			the_content(); 
+		else :
+			?><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a><?php
+			the_excerpt();
+		endif;
+	?>
 </div>

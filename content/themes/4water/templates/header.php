6a27ff1d439f79f0d8640050d8917e679d85dbc8
Iain Porter <iporter@intraspin.com>
2014-06-02 10:47:50 +0100

    Commenting out the bands banner ad and Facebook CTA

--- content/themes/4water/templates/header.php
+++ content/themes/4water/templates/header.php
@@ -3,7 +3,6 @@
 		<a class="logo" href="<?php echo home_url(); ?>/">
 			<?php $img = wp_get_attachment_image_src( get_field('logo', 'options'), 'full'); ?>
 			<img src="<?php echo $img[0]; ?>" alt="4water">
-			<?php // img('logo.png', '4water logo'); ?>
 		</a>
 		<nav id="primary" class="primary" role="navigation">
 			<?php if (has_nav_menu('primary_navigation'))	wp_nav_menu(array('theme_location' => 'primary_navigation')); ?>
@@ -24,6 +23,7 @@
 				<li><a href="#">4water Menu</a></li>
 			<?php endif; ?>
 		</ul>
+		<!--
 		<a class="bannerad" href="/shop/">
 			<h3>Show your support</h3>
 			<p>only &pound;1 each and 60p goes to charity</p>
@@ -32,5 +32,6 @@
 		<div class="extended-cta">
 			<p>and <a href="http://www.facebook.com/sharer.php?s=100&p[title]=4WATER&p[summary]=Summary content...&p[url]=http://weare4water.org&p[images][0]=YOUR_IMAGE_TO_SHARE_OBJECT">share our story</a> so everyone can benefit</p>
 		</div>
+		-->
 	</div>
 </header>
